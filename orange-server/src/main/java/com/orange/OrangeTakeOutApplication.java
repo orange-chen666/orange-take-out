bb0018f4c3f147d7091a1d7c375cfdc2e105bda9
orange <1355488736@qq.com>
2025-10-27 21:20:47 +0800

    桔子外卖

---
+++ orange-server/src/main/java/com/orange/OrangeTakeOutApplication.java
@@ -0,0 +1,13 @@
+package com.orange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeTakeOutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrangeTakeOutApplication.class, args);
+    }
+
+}
